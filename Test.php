2e861f0e44a506436d4b8f20b664720c2b070ff3
DanielCodec <danielawujoola1@gmail.com>
2017-08-01 14:16:44 +0100

    Update Test.php

--- Test.php
+++ Test.php
@@ -5,14 +5,7 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
-	
-// 	public function assertEquals($a,$b){
-// 	if($a==$b){
-// 	 return true;
-// 		}
-// 		return false;
-		
-// 	}
+
 	
 }
 

1ea077903b9e21fb3442718d3dd3c137d708e123
DanielCodec <danielawujoola1@gmail.com>
2017-08-01 11:12:46 +0100

    Update Test.php

--- Test.php
+++ Test.php
@@ -5,10 +5,7 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
-	public function show($text){
-	return echo $text;
-	}
-	$this->show('I am a boy');
+	
 }
 
 ?>
